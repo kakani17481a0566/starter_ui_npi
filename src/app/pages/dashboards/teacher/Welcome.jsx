701d41e37885f94ede5b875302effbacea135f17
SaivardhanClasses <bablugone4@gmail.com>
2025-07-15 10:15:53 +0530

    added name in card

--- src/app/pages/dashboards/teacher/Welcome.jsx
+++ src/app/pages/dashboards/teacher/Welcome.jsx
@@ -2,14 +2,16 @@
 import clsx from "clsx";
 
 // Local Imports
-import { Button, Card } from "components/ui";
+import {  Card } from "components/ui";
 import { useBreakpointsContext } from "app/contexts/breakpoint/context";
 import TeacherWelcome from "assets/illustrations/teacher-welcome.svg?react";
+import { getSessionData } from "utils/sessionStorage";
 
 // ----------------------------------------------------------------------
 
 export function Welcome() {
   const { smAndUp } = useBreakpointsContext();
+  const {user}=getSessionData();
   return (
     <Card
       className={clsx(
@@ -23,22 +25,22 @@ export function Welcome() {
 
       <div className="mt-2 flex-1 pt-2 text-center text-white sm:mt-0 sm:text-start">
         <h3 className="text-xl">
-          Welcome Back, <span className="font-semibold">Caleb</span>
+          Welcome Back, <span className="font-semibold">{user}</span>
         </h3>
-        <p className="mt-2 leading-relaxed">
+        {/* <p className="mt-2 leading-relaxed">
           Your student completed
           <span className="font-semibold text-black/90"> 85%</span> of tasks
         </p>
         <p>
           Progress is <span className="font-semibold">excellent!</span>
-        </p>
+        </p> */}
 
-        <Button
+        {/* <Button
           unstyled
           className="mt-6 rounded-lg bg-white/90 px-5 py-2.5 text-gray-900 hover:bg-white/70 focus:bg-white/70"
         >
           View Lessons
-        </Button>
+        </Button> */}
       </div>
     </Card>
   );
