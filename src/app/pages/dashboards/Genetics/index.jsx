0f2488f90a237ceef5e871168f39261ec20d7724
kakani17481a0566 <84347625+kakani17481a0566@users.noreply.github.com>
2025-10-16 14:22:44 +0530

    Update index.jsx

--- src/app/pages/dashboards/Genetics/index.jsx
+++ src/app/pages/dashboards/Genetics/index.jsx
@@ -5,9 +5,6 @@ export default function Genetics() {
   return (
     <Page title="Example">
       <div className="transition-content w-full  pt-5 lg:pt-6">
-        <div className="min-w-0">
-
-        </div>
         <NewPostFrom />
       </div>
     </Page>
